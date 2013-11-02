276c0ff5f05a3a4187b39e21a6648405b0e48574
Franck De Goër <degoerdf@ensimag.fr>
2013-11-02 12:34:12 +0100

    Using template for test

--- website/gallery/views.py
+++ website/gallery/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    template_name = "gallery/index.html"
+    return render(request, template_name) 
 
 def detail(request, question_id):
         return HttpResponse("You're looking at question %s." % question_id)
