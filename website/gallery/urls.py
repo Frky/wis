72d7e2b7880f4428ee32571ec4b83c2ca8cc710d
Franck De Goër <degoerdf@ensimag.fr>
2013-11-23 20:58:27 +0100

    Bug fix on url dispatcher

--- website/gallery/urls.py
+++ website/gallery/urls.py
@@ -12,5 +12,5 @@ urlpatterns = patterns('wis.apps.gallery.views',
                        url(r'^(?P<user>[-A-Za-z0-9_]+)/(?P<gallery_slug>(.+))$',
                            gallery_home,
                            name="wis_gallery_home"),
-                       ),
+                       )
 
