ec5367f1d9b9dcd745823deb7f22dd781b31e9ac
Franck De Goër <degoerdf@ensimag.fr>
2013-11-23 20:54:09 +0100

    Bug fix on url dispatcher

--- website/gallery/urls.py
+++ website/gallery/urls.py
@@ -3,13 +3,14 @@ from gallery.views import *
 
 
 urlpatterns = patterns('wis.apps.gallery.views',
-                       # Galleries url
-                       url(r'^(?P<user>[-A-Za-z0-9_]+)/(?P<gallery_slug>(.+))$',
-                           gallery_home,
-                           name="wis_gallery_home"),
-
                        # User galleries
                        url(r'^(?P<user>[-A-Za-z0-9_]+)$',
                            user_galleries,
                            name="wis_user_gallery"),
                        )
+
+                       # Galleries url
+                       url(r'^(?P<user>[-A-Za-z0-9_]+)/(?P<gallery_slug>(.+))$',
+                           gallery_home,
+                           name="wis_gallery_home"),
+
